9348f650208f87206c975346f97d6ce43409aa6a
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2023-11-27 21:52:12 +0300

    Added ArrayCopyAlignedBenchmark ArrayCopyUnalignedBoth ArrayCopyUnalignedDst ArrayCopyUnalignedSrc

---
+++ microbenchmarks/src/androidTest/java/com/example/blueteammicrobenchmark/ArrayCopyUnalignedDst.java
@@ -0,0 +1,66 @@
+package com.example.blueteammicrobenchmark;
+
+import androidx.benchmark.BenchmarkState;
+import androidx.benchmark.junit4.BenchmarkRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Random;
+
+@RunWith(AndroidJUnit4.class)
+public class ArrayCopyUnalignedDst {
+    @Rule
+    public BenchmarkRule benchmarkRule = new BenchmarkRule();
+
+    int length = 310;
+
+    // Dst position unaligned
+    int fromPos = 8;
+    int toPos = 9;
+    char[] fromCharArr = new char[length];
+    int[] fromIntArr = new int[length];
+    long[] fromLongArr = new long[length];
+
+    private void measure(Runnable f) {
+        BenchmarkState localState = benchmarkRule.getState();
+        while(localState.keepRunningInline()) {
+            f.run();
+        }
+    }
+
+    @Test
+    public void testChar() {
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            fromCharArr[i] = (char)(random.nextInt(26) + 'a');
+        }
+        char[] toCharArr = new char[length];
+
+        measure(() -> System.arraycopy(fromCharArr, fromPos, toCharArr, toPos, length-10));
+    }
+
+    @Test
+    public void testInt() {
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            fromIntArr[i] = random.nextInt();
+        }
+        int[] toIntArr = new int[length];
+
+        measure(() -> System.arraycopy(fromIntArr, fromPos, toIntArr, toPos, length-10));
+    }
+
+    @Test
+    public void testLong() {
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            fromLongArr[i] = random.nextLong();
+        }
+        long[] toLongArr = new long[length];
+
+        measure(() -> System.arraycopy(fromLongArr, fromPos, toLongArr, toPos, length-10));
+    }
+}
